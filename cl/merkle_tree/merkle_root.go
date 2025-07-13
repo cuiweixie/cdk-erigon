e75de912f61026c067dfaa93e1f60ba0ba8512fc
cuiweixie <cuiweixie@gmail.com>
2025-07-14 00:47:41 +0800

    refactor: using unsafe.Slice/SliceData instead of reflect.SliceHeader

--- cl/merkle_tree/merkle_root.go
+++ cl/merkle_tree/merkle_root.go
@@ -4,7 +4,6 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"reflect"
 	"unsafe"
 
 	"github.com/ledgerwatch/erigon-lib/common"
@@ -92,18 +91,8 @@ func HashByteSlice(out, in []byte) error {
 
 func convertHeader(xs []byte) [][32]byte {
 	// i wont pretend to understand, but my solution for the problem is as so
-
-	// first i grab the slice header of the input
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&xs))
-	// then i allocate a new result slice of no size - this should make the escape analyzer happy i think?
-	dat := make([][32]byte, 0)
-	// we then get the header of our output  to modify
-	chunkedHeader := (*reflect.SliceHeader)(unsafe.Pointer(&dat))
-	// then we move over the values
-	chunkedHeader.Len = header.Len / 32
-	chunkedHeader.Cap = header.Cap / 32
-	chunkedHeader.Data = header.Data
-	return dat
+	xsLen, xsData := len(xs), unsafe.SliceData(xs)
+	return unsafe.Slice((*[32]byte)(unsafe.Pointer(xsData)), xsLen/32)
 }
 
 func MerkleRootFromFlatLeaves(leaves []byte, out []byte) (err error) {
--- consensus/ethash/ethash.go
+++ consensus/ethash/ethash.go
@@ -24,7 +24,6 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
-	"reflect"
 	"runtime"
 	"strconv"
 	"sync"
@@ -121,13 +120,7 @@ func memoryMapFile(file *os.File, write bool) (mmap.MMap, []uint32, error) {
 		return nil, nil, err
 	}
 	// The file is now memory-mapped. Create a []uint32 view of the file.
-	var view []uint32
-	header := (*reflect.SliceHeader)(unsafe.Pointer(&view))
-	header.Data = (*reflect.SliceHeader)(unsafe.Pointer(&mem)).Data
-	header.Len /= 4
-	header.Cap /= 4
-
-	return mem, view, nil
+	return mem, unsafe.Slice((*uint32)(unsafe.Pointer(unsafe.SliceData(mem))), len(mem)/4), nil
 }
 
 // memoryMapAndGenerate tries to memory map a temporary file of uint32s for write
